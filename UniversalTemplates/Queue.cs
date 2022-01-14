16e51d3841439b2a727a22a85182fcc94065fe87
nightwills <netherwillshot@gmail.com>
2022-01-15 02:57:07 +0300

    Добавьте файлы проекта.

---
+++ UniversalTemplates/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UniversalTemplates
+{   
+    public class Queue<T>
+    {
+        private int Head = -1;
+        private int Rear = -1;
+        private int Count = 0;
+        private readonly int Size;
+        private readonly T[] Array;
+        public Queue(int Size)
+        {
+            this.Size = Size;
+            this.Array = new T[Size];
+        }
+        public bool IsFull()
+        {
+            return this.Rear == this.Size - 1;
+        }
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public void Add(T Item)
+        {
+            if (this.IsFull())
+                throw new Exception("Очередь полностью заполнена.");
+            this.Array[++this.Rear] = Item;
+            this.Count++;
+        }
+        public T Get()
+        {
+            if (this.IsEmpty())
+                throw new Exception("Очередь не заполнена.");
+            T value = this.Array[++this.Head];
+            this.Count--;
+            if (this.Head == this.Rear)
+            {
+                this.Head = -1;
+                this.Rear = -1;
+            }
+            return value;
+        }
+    }
+}
---
+++ UniversalTemplates/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalTemplates
+{
+    public class Stack<T>
+    {
+        private List<T> _Items = new List<T>();
+        public int Count => _Items.Count;
+        public void Push(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            this._Items.Add(item);
+        }
+        public T Pop()
+        {
+            var item = this._Items.LastOrDefault();
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пуст. Нет элементов для получения.");
+            }
+            this._Items.RemoveAt(this._Items.Count - 1);
+            return item;
+        }
+        public T Peek()
+        {
+            var item = this._Items.LastOrDefault();
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пуст. Нет элементов для получения.");
+            }
+            return item;
+        }
+    }
+}
